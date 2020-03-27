f18424fa105e8d1629b2e1a5fb0036087496b474
vinyshoow <viniciuslopes.qz@hotmail.com>
2020-03-27 14:35:08 -0300

    Criando autenticação das Rotas / testes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors} = require('celebrate');
 const routes = require('./routes');
 
 
@@ -8,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 
 
-app.listen(3333);
+module.exports = app;
